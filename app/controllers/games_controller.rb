86ce285bf81ce992dfefd62f4e0265165dc7f9ab
Paul Tschudin <paul.tschudin@gmail.com>
2020-01-07 21:21:08 -0600

    made minor updates to make minorly ready for game play

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,20 +4,18 @@ class GamesController < ApplicationController
   end
 
   def gameLobby
-    @users = User.where(player: true)
     @game_code = params[:game_code]
+    @game_code = @game_code.upcase
+    @users = User.where(player: true)
     @round = params[:round].to_i
     @round = @round + 1
   end
 
   def guessingRound
+    @users = User.where(player: true)
     @game_code = params[:game_code]
     @round = params[:round]
     @submissions = Submission.where(code: @game_code, round: @round)
-  end
-
-  def updateRound
-
   end
 
 end
